473674c7179db5c7efb4b32d30083a712b5c0d8a
TonyGaona <73232497+TonyGaona@users.noreply.github.com>
2022-11-08 10:53:42 -0500

    cambio 8nov

--- index.php
+++ index.php
@@ -1,61 +1,1 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Happy Pets</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="icon" href="images/logo.png" sizes="32x32" />
-    <link rel="icon" href="images/logo.png" sizes="192x192" />
-    <link rel="apple-touch-icon" href="images/logo.png" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-</head>
-<body>
-<div class="contenedor">
-	<header class="cabeceraPrincipal">
-		<section class="logotipo"><img src="images/logotipoPet.png"></section>
-		<nav class="menuPrincipal">
-			<a href="#">Inicio</a>
-			<a href="#">Servicios</a>
-			<a href="#">Productos</a>
-			<a href="#">Adopción</a>
-			<a href="#">Contactos</a>
-		</nav>
-	</header>
-	<section class="sliderPrincipal">
-		<img src="images/dogs.jpg">
-	</section>
-	<section class="titulo">
-		<h2>Adopcion</h2>
-	</section>
-	<section class="adopcion1">
-		<img src="images/dog1.jpg">
-		<section class="cardog">
-			<h3>firulais</h3>
-			<p>El perro,llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho,  tuso,  choco,  entre otros; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis.
-			</p>
-			<button>Mas informacion</button>
-		</section>
-	</section>
-	<section class="adopcion2"><h3>Noticia 2</h3></section>
-	<section class="doctor">
-		<h3>Doctores</h3>
-	</section>
-	<section class="sponsor">
-		<h3>Sponsor</h3>
-	</section>
-	<footer class="piePagina">
-		<section class="derechos">
-			<h6>Derechos Reservados UTPL 2022</h6>
-		</section>
-		<nav class="redes">
-			<a href=""><i class="fa-brands fa-facebook"></i></a>
-			<a href=""><i class="fa-brands fa-instagram"></i></a>
-			<a href=""><i class="fa-brands fa-tiktok"></i></a>
-		</nav>
-	</footer>
-</div>
-</body>
-</html>
+<!DOCTYPE html><html><head>	<meta charset="utf-8">	<meta name="viewport" content="width=device-width, initial-scale=1">	<title>Happy Pets</title>	<link rel="stylesheet" type="text/css" href="css/style.css">	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">	<link rel="icon" href="images/logo.png" sizes="32x32" />    <link rel="icon" href="images/logo.png" sizes="192x192" />    <link rel="apple-touch-icon" href="images/logo.png" />    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" /></head><body><div class="contenedor">	<header class="cabeceraPrincipal">		<section class="logotipo"><img src="images/logotipoPet.png"></section>		<nav class="menuPrincipal">			<a href="#">Inicio</a>			<a href="#">Servicios</a>			<a href="#">Productos</a>			<a href="#">Adopción</a>			<a href="#">Contactos</a>		</nav>	</header>	<section class="sliderPrincipal">		<img src="images/dogs.jpg">	</section>	<section class="titulo">		<h2>Adopcion</h2>	</section>	<section class="adopcion1">		<img src="images/dog1.jpg">		<section class="cardog">			<h3>Firulais</h3>			<p>El perro,llamado perro doméstico o can, y en algunos lugares coloquialmente llamado chucho,  tuso,  choco,  entre otros; es un mamífero carnívoro de la familia de los cánidos, que constituye una especie del género Canis.			</p>			<button>Mas informacion</button>		</section>	</section>	<section class="adopcion2">		<img src="images/cat1.jpg">		<section class="carcat">			<h3>Tommy</h3>			<p>El gato doméstico, llamado más comúnmente gato, y de forma coloquial minino,  michino,  michi,  micho, mizo,  miz,  morroño o morrongo,  y algunos nombres más, es un mamífero carnívoro de la familia Felidae. Es una subespecie domesticada, por la convivencia con el ser humano, del gato montés			</p>			<button>Mas informacion</button>		</section>	</section>	<section class="titulo2">		<h2>Doctores</h2>	</section>	<section class="vets">		<section class="vet1">		<img src="images/vet1.jpg">		<section class="vet10">			<h3>Johnny Sins</h3>			<p> Doctor mas joven con experiencia en disntiston campos			</p>			<button>Mas informacion</button>		</section>	</section>	<section class="vet2">		<img src="images/vet2.jpg">		<section class="vet20">			<h3>Johnny Sins</h3>			<p> Doctor mas joven con experiencia en disntiston campos			</p>			<button>Mas informacion</button>		</section>	</section>	<section class="vet3">		<img src="images/vet3.jpg">		<section class="vet30">			<h3>Johnny Sins</h3>			<p> Doctor mas joven con experiencia en disntiston campos			</p>			<button>Mas informacion</button>		</section>	</section>	</section>		<section class="sponsor">		<h3>Sponsor</h3>	</section>	<footer class="piePagina">		<section class="derechos">			<h6>Derechos Reservados UTPL 2022</h6>		</section>		<nav class="redes">			<a href=""><i class="fa-brands fa-facebook"></i></a>			<a href=""><i class="fa-brands fa-instagram"></i></a>			<a href=""><i class="fa-brands fa-tiktok"></i></a>		</nav>	</footer></div></body></html>
